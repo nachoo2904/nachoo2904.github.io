3039ab17eb93e5af727e82be986b69186218e339
Ignacio <nachoasena@gmail.com>
2023-02-27 17:01:50 -0300

    Trabajo final Argentina Programa 4.0

---
+++ index.js
@@ -0,0 +1,17 @@
+
+document.getElementById("exp1").addEventListener ("click", function (){
+    document.getElementById("verExp1").style.display ="block" ;
+});
+
+document.getElementById("ocultarExp1").addEventListener ("click", function (){
+    document.getElementById("verExp1").style.display ="none" ;
+});
+
+document.getElementById("exp2").addEventListener ("click", function (){
+    document.getElementById("verExp2").style.display ="block" ;
+});
+
+document.getElementById("ocultarExp2").addEventListener ("click", function (){
+    document.getElementById("verExp2").style.display ="none" ;
+});
+
